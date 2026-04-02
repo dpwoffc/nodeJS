a308f5fb2c5133874581863bd18ead184731bae0
@dpwoffc | Dwi Putra Wibowo <dwiputrawibowo2009@gmail.com>
2026-04-02 13:11:58 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = require("./route");
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Server running http://localhost:3000');
+});
+
